f79d900ace6a6ad27cc56a810b08a8e78be44cf6
soo-essoklina-ulrich <essoklina.soo@ipnetinstitute.com>
2024-05-12 20:38:59 +0000

    deuxieme commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import {ProfileComponent} from "./profile/profile.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '**', redirectTo: 'pro', pathMatch: 'full' },
+  { path: 'pro', component: ProfileComponent },
+];
